3536a5054e8a9c05923c0413401f4ff46c8b8f35
Christopher Marti <chrisjm093@gmail.com>
2020-08-26 15:47:32 -0700

    Server configuration for Heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const routes = require("./routes")
 
 //Middleware defined
 app.use(express.urlencoded({extended: true}));
@@ -11,13 +12,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err){
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
+app.use(routes);
 
 //Start the server
 app.listen(PORT, function() {
