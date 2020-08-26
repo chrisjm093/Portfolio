4e431f0018371599ab742be0c149b078b9bb68c7
Christopher Marti <chrisjm093@gmail.com>
2020-08-26 15:08:22 -0700

    Configuration for Heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname, '/client/public/index.html'), function(err){
+  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err){
     if (err) {
       res.status(500).send(err)
     }
