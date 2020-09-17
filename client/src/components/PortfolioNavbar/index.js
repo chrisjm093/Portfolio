f2a55cb9b667a273b23d3b07def3cdef02337c84
Christopher Marti <chrisjm093@gmail.com>
2020-09-17 14:35:32 -0700

    Fixed media query styling error that was making links disappear on smaller screen sizes.

--- client/src/components/PortfolioNavbar/index.js
+++ client/src/components/PortfolioNavbar/index.js
@@ -9,18 +9,18 @@ function PortfolioNavbar(){
     <Navbar expand="lg" navbar="light">
     <Container fluid="lg">
     <Navbar.Brand href="/">Christopher Marti</Navbar.Brand>
-    <Navbar.Collapse id="basic-navbar-nav">
+   
       <Nav className="mr-auto">
-        <NavDropdown title="Links" id="basic-nav-dropdown">
+        <NavDropdown className="navItem" title="Links" id="basic-nav-dropdown">
           <NavDropdown.Item  href="https://drive.google.com/file/d/1jJ83ZhwWns-9nhpRyJV84rRxRLpbGRE1/view?ths=true">Resume</NavDropdown.Item>
           <NavDropdown.Item  href="https://github.com/chrisjm093">GitHub</NavDropdown.Item>
           <NavDropdown.Item  href="https://www.linkedin.com/in/christopher-m-25246649">LinkedIn</NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link href="/">About</Nav.Link>
-        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-        <Nav.Link href="/contact">Contact</Nav.Link>
+        <Nav.Link className="navItem" href="/">About</Nav.Link>
+        <Nav.Link className="navItem" href="/portfolio">Portfolio</Nav.Link>
+        <Nav.Link className="navItem" href="/contact">Contact</Nav.Link>
       </Nav>  
-    </Navbar.Collapse>
+   
     </Container>
 </Navbar>
 
